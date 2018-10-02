9a716e8672b5b49ff4bf136835a0af0e39fbd5b0
sozkaynak <sumeyye.ozkaynak@ogr.dpu.edu.tr>
2018-10-02 11:42:19 +0000

    sunucu değişiklikleri

--- sumeyyeozkaynak/settings/production.py
+++ sumeyyeozkaynak/settings/production.py
@@ -1,15 +1,15 @@
 from sumeyyeozkaynak.settings.base import *
 
 DEBUG = False
-ALLOWED_HOSTS=['www.sumeyyeozkaynak.me', 'sumeyyeozkaynak.me','188.166.68.147']
+ALLOWED_HOSTS=['www.sumeyyeozkaynak.me', 'sumeyyeozkaynak.me','188.166.122.13']
 
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'django_sumeyyeozkaynak',
+        'NAME': 'django_website',
         'USER': 'sumeyyeozkaynak',
-        'PASSWORD': 'djsmyyzkynkpassword',
+        'PASSWORD': '203948',
         'HOST': 'localhost',
         'PORT': '',
     }
